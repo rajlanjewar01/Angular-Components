b540c3e1341b38402f16868940d6708fbc069bdb
rajperficient01 <rajl.perficient@gmail.com>
2022-04-20 15:11:35 +0530

    new final update commit

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-mods-home',
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mods-home.component.css']
 })
 export class ModsHomeComponent implements OnInit {
+  
+  items = [
+    {
+      title: 'What is a dog?',
+      content: 'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.'
+    },
+    {
+      title: 'What kinds of dogs are there?',
+      content: 'There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.'
+    },
+    {
+      title: 'How do you acquire a dog?',
+      content: 'Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters. A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your dog from a shelter, helps give a good home to a dog who may not find one so readily.'
+    }
+  ];
+
+  modalOpen = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showmdl(){
+    this.modalOpen = !this.modalOpen;
   }
 
 }
